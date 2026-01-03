0a60d6278dedc2861641d276c11f27fa59ebc33b
kostya <kt5082206@gmail.com>
2026-01-03 05:42:50 +0000

    updt 123

--- cousework14/codeMain/views/logout.py
+++
@@ -1,7 +0,0 @@
-# views.py
-from django.contrib.auth import logout
-from django.shortcuts import redirect
-
-def logout_view(request):
-    logout(request)
-    return redirect('home')  # або куди хочеш після виходу
